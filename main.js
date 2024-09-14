5013dba4b8778347f3c5485fecad033c2a5e2b0c
Small-eze-1234 <dknlezekiel@gmail.com>
2024-09-14 07:01:37 -0700

    javascript begining

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("welcome to javascript")
+console.log("hello world!!!")
+
+// var let contant
+
+var userName = "Darasimi"
+let email ="dklezekiel@gmail.com"
+var password ="1234"
+const gender ="female"
+var Status ="student"
+var age =14
+console.log(userName)
+console.log(email)
+
+var userName = "Darasimi shriffdeen"
+userName="Darasimi sheriffdeenz"
+Status="developer"
+console.log(Status)
+console.log(userName)
+var age =22
+console.log(age)
+

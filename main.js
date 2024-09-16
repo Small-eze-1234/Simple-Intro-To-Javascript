2ac0184f930e0b06627d4eede32bbb7f8156ac0d
Small-eze-1234 <dknlezekiel@gmail.com>
2024-09-16 03:01:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -19,4 +19,59 @@ console.log(Status)
 console.log(userName)
 var age =22
 console.log(age)
+// data types
+// promitive data type 
+// String
+// number
+// boolean
+// complex data tpe
+// object and arrays
+// function
+// null
+// Symbol
+var firstName =" Adewale "
+var age = 16.7
+var isOnline = true
+var brain = null
+console.log(typeof(brain))
+console.log(typeof(isOnline))
+console.log(firstName.length)
+console.log(firstName[0])
+console.log(firstName.charAt(0))
+var newFirstname=firstName.trim()
+console.log(newFirstname.length)
+
+// index
+
+console.log(firstName.indexOf('e'))
+console.log(firstName.lastIndexOf('e'))
+let sentence = 'Welcome to appclick'
+// console.log(sentence.search('Appclick'))
+// console.log(sentence.toUpperCase())
+console.log(sentence.indexOf('APPclick'.toLowerCase()))
+console.log(sentence.toUpperCase())
+var ModifiedSentence = sentence.replace('Welcome' ,'Go')
+console.log(ModifiedSentence);
+// string concatination
+console.log("my name is " + "and i am " + age +" year's old")
+// string interpolation
+console.log(`my name is ${firstName} and i am ${age} year's old`)
+var fav_food ="Rice"
+var fav_car = "Benz"
+var fav_car_location = "Apete"
+console.log(`Mr Bala's favourite food is ${fav_food} and his favourite car spot is ${fav_car} the name of his car name his ${fav_car_location}`)
+// let password ='winner'
+
+
+var number1 = 65
+var number2 =32.2
+// operators
+// arithmetic operators
+// + addition
+// - subtraction
+// * multiplication
+// / division
+// % floor 
+// ** exponential
+console.log(2 ** 3)
 
